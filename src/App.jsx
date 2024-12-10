9dca0ef1d83ba380aed7ba36682542bc126ca6e9
Brendon Luicien <brendonl@missionreadyhq.com>
2024-12-10 19:58:35 +1300

    added some html

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>THIS WILL BE DEPLOYED</h1>
+      <h2 style={{backgroundColor: 'deeppink', color: 'deepskyblue'}} >This has been added after the netlify deploy</h2>
     </>
   )
 }
