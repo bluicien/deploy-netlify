aa11ebe8a9ae76c1ff944f62f817bb13baf4caec
Brendon Luicien <brendonl@missionreadyhq.com>
2024-12-10 20:17:22 +1300

    added deploy to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <h1>THIS WILL BE DEPLOYED</h1>
       <h2 style={{backgroundColor: 'deeppink', color: 'deepskyblue'}} >This has been added after the netlify deploy</h2>
+      <h3 style={{backgroundColor: 'whitesmoke', color: 'deepskyblue'}} >This his now also deployed on vercel</h3>
     </>
   )
 }
